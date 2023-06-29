bd4b08fcaf94c4c7f771397cf89b5080cec27852
JulianmLacey <Julianmlacey@gmail.com>
2023-06-29 14:07:23 -0700

    Added Comments

--- script.js
+++ script.js
@@ -1,139 +1,82 @@
-// Assignment Code
+/*
+Julian Lacey
+6/29/23
+
+Javascript for password generator
+*/
+
+//Entry point when generate button selected
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
+//get desired password length from the user
+var getLength = function () {
+  var length = Number(prompt("How Many Characters?", "Enter Length"));
 
-//get desired length from the user
-var getLength = function(){
-  var length = Number(prompt("How Many Characters?", "Enter Length"))
-  if((length<8)||(length>128)){
+  //check to make sure its a valid length
+  if (length < 8 || length > 128) {
     alert("Invalid Length");
     return getLength();
   }
-  console.log(typeof length);
-  console.log(length);
+
   return length;
+};
 
-}
-
-var promptReqs = function(){
+//get password character requrements from user
+var promptReqs = function () {
+  //cat each answer into a string for verification and character selection
   var req = "";
   req += prompt("Lowercase Letters", "y/n");
   req += prompt("uppercase Letters", "y/n");
   req += prompt("numeric Letters", "y/n");
   req += prompt("specialChar Letters", "y/n");
 
-  console.log(typeof req);
-  console.log(req);
-  if(req=="nnnn"){
-    alert("Atleast One Character Type Must Be Used");
+  //verify atleast one character type is included
+  //if none are included re-prompt the user, otherwise return the string
+  if (req[0] != "y" && req[1] != "y" && req[2] != "y" && req[3] != "y") {
+    window.alert("Atleast One Character Type Must Be Used");
     return promptRequirements();
-  }else{
-    return req;
   }
 
-  
-}
+  return req;
+};
 
-var buildPassword = function(passLength, reqs){
-  var chars = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", "!@#$%^&*()"];
-  var charsValid = " ";
-  for(let i = 0; i<4;i++){
-    if(reqs[i]=="y"){
-      charsValid+=chars[i];
-    }
+//Create the password
+var buildPassword = function (passLength, reqs) {
+  var chars = [
+    "abcdefghijklmnopqrstuvwxyz",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "0123456789",
+    "!@#$%^&*()",
+  ];
+  var charsValid = "",
+    passStr = "";
+
+  //iterate over the string of requirements
+  //if user answered y, concatonate the corresponding character types from the chars arrar
+  //to the valid character string
+  for (let i = 0; i < 4; i++) {
+    if (reqs[i] == "y") charsValid += chars[i];
   }
-  
-  console.log(passLength);
-  console.log(reqs);
-  var passStr = "";
 
-  for(var j = 0; j<passLength;j++){
-    passStr+=charsValid[Math.floor(Math.random() * charsValid.length)];
+  //concatonate the desired number of characters to the password, randomly choosing a character from the
+  //valid characters string
+  for (var j = 0; j < passLength; j++) {
+    passStr += charsValid[Math.floor(Math.random() * charsValid.length)];
   }
 
-  console.log(passStr);
   return passStr;
-}
+};
 
-
-
+/*--------MAIN Function----------*/
+//write the password the the page
 function writePassword() {
-  //var password = generatePassword();
+  //get the password object from the browser
   var passwordText = document.querySelector("#password");
-  var len = getLength();
-  var reqs = promptReqs();
-  passwordText.value = buildPassword(len, reqs);
 
+  //get user password requrements
+  //Build the password, then set the password value equal to the generated string
+  passwordText.value = buildPassword(getLength(), promptReqs());
 
-
-  return ;
+  return passwordText.value;
 }
-
-
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-// Add event listener to generate button
-
-
-/*
-function generatePassword(){
-  var length = Number(prompt("How Many Characters?", "Enter Length"))
-  if((length<8)||(length>128)){
-    alert("Invalid Length");
-    return generatePassword();
-  }
-  console.log(typeof length);
-  console.log(length);
-
-  var req = promptRequirements();
-  var pass = buildPassword(length, req);
-  console.log(pass);
-  return pass;
-}
-
-
-
-function promptRequirements(){
-  var req = "";
-  req += prompt("Lowercase Letters", "y/n");
-  req += prompt("uppercase Letters", "y/n");
-  req += prompt("numeric Letters", "y/n");
-  req += prompt("specialChar Letters", "y/n");
-
-  console.log(req);
-  if(req=="nnnn"){
-    alert("Atleast One Character Type Must Be Used");
-    return promptRequirements();
-  }else{
-    return req;
-  }
-  
-}
-
-function buildPassword(len, reqs){
-  var chars = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", "!@#$%^&*()"];
-  var charsValid = " ";
-  for(let i = 0; i<4;i++){
-    if(reqs[i]=="y"){
-      charsValid+=chars[i];
-    }
-  }
-  console.log(charsValid);
-  var passStr = " ";
-  for(let i = 0; i<len;i++){
-    passStr += charsValid[Math.random(charsValid.length-1)];
-  }
-  console.log(passStr);
-  return passStr;
-}
-*/
-//8-128 characters
-//lowercase, uppercase, numeric, special characters
-//must have atlease one
-//generate password
